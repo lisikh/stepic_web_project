bbbb2876289e081dbeb2a07c672f9a39132a40d4
Sergey Lisikh <s.lisikh@crystals.ru>
2016-11-11 12:00:31 +0300

    configure nginx for reverse proxy

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my_project.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/my_project.conf
+sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
