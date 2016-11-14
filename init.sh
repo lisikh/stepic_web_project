31e80254e8589905cff52171ef0c6565faab5ed5
Sergey Lisikh <s.lisikh@crystals.ru>
2016-11-14 17:58:08 +0300

    Add QA django project

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my_project.conf
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/my_project.conf
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.conf
+sudo ln -s /home/box/web/etc/qa.conf /etc/gunicorn.d/qa.conf
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
