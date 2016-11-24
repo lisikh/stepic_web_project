967e72042dd6ab866544f57cd314a59142bd8c64
Sergey Lisikh <s.lisikh@crystals.ru>
2016-11-24 21:51:31 +0300

    add auth

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo ln -s /home/box/web/etc/qa.conf /etc/gunicorn.d/qa.conf
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql restart
-mysql -uroot -e "create database qa"
+mysql -uroot -e "create database qa charset=utf8"
 mysql -uroot -e "create user 'sa' identified by 'qa'"
 mysql -uroot -e "grant all on qa.* to 'sa'"
 /home/box/web/ask/manage.py syncdb
